be0507bebdf67f6b97e7f0f7cb946ddaf3e54c84
lazar <jaekll@126.com>
2018-08-28 18:30:27 +0800

    menu

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Home from '@/views/Home'
 import Main from '@/views/Main'
 import Login from '@/views/login/Login'
 import UserList from '@/views/user/List'
+import MenuList from '@/views/system/menu/List'
+import MenuAdd from '@/views/system/menu/List'
+import MenuEdit from '@/views/system/menu/List'
 
 Vue.use(Router)
 
@@ -11,23 +14,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
+      name: '主页',
       component: Home,
       iconCls:'el-icon-message',
       children:[
-        {path:'/main',component:Main,name:'主业',hidden:true},
-        {path:'/user',component:UserList,name:'用户列表'}
+        {path:'/main',component:Main,name:'主业'},
       ]
     },
     {
-    path: '/home',
-    component: Home,
-    children: [
-      { path: 'menu/list', component: menuList, name: 'menuList', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }},
-      { path: 'menu/add', component: menuAdd, name: 'menuAdd', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }},
-      { path: 'menu/edit/:id', component: menuEdit, name: 'menuEdit', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }}
-    ]
-  },
+      path: '/',
+      name:'菜单',
+      iconCls:'el-icon-menu',
+      component: Home,
+      children: [
+        { path: 'menu/list', component: MenuList, name: 'MenuList', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }},
+        { path: 'menu/add', component: MenuAdd, name: 'MenuAdd', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }},
+        { path: 'menu/edit/:id', component: MenuEdit, name: 'MenuEdit', meta: { hideLeft: false, module: 'Administrative', menu: 'menu' }}
+      ]
+    },
+    {
+        path: '/',
+        name:'用户管理',
+        iconCls:'el-icon-setting',
+        component: Home,
+        children: [
+           {path:'user/list',component:UserList,name:'用户列表'}
+        ]
+    },
     {
         path:'/login',
         name:Login,
